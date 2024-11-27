4129dde908b8d6c3fb9877a65bacf1e5b2bcc492
Mia <mia.voss.16@gmail.com>
2024-11-27 01:13:33 +0100

    pray

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,29 @@
+// /** @type {import('next').NextConfig} */
+// // const isProd = process.env.NODE_ENV === 'production';
+// const nextConfig = {
+//   reactStrictMode: true,
+//   images: {
+//     unoptimized: true, // Disable default image optimization
+//   },
+//   assetPrefix: 'https://ccgv4.github.io/',
+// //   basePath: '',
+//   output: 'export',
+//   distDir: 'docs'
+// };
+
+// export default nextConfig;
+
 /** @type {import('next').NextConfig} */
 // const isProd = process.env.NODE_ENV === 'production';
 const nextConfig = {
-  reactStrictMode: true,
-  images: {
-    unoptimized: true, // Disable default image optimization
-  },
-  assetPrefix: 'https://ccgv4.github.io/',
-//   basePath: '',
-  output: 'export',
-  distDir: 'docs'
-};
-
-export default nextConfig;
+    reactStrictMode: true,
+    images: {
+      unoptimized: true, // Disable default image optimization
+    },
+    // assetPrefix: './',
+    // basePath: '/.',
+    output: 'export',
+    distDir: 'docs'
+  };
+  
+  export default nextConfig;
